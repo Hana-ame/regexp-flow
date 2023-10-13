89bc81ace2f639a9d52e19109d997ff4f921c08e
lumino <luminovoez@gmail.com>
2023-10-14 07:27:03 +0800

    双向绑定的patternArray不起作用

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import Block from './components/Block.vue'
-import { range } from './functions/function'
+import Block from '@/components/Block.vue'
+import { range, hasItem, saveItem, loadItem } from '@/functions/helpers'
 
 import { ref } from 'vue';
 import { NInput, NButton } from 'naive-ui'
@@ -8,14 +8,11 @@ import { NInput, NButton } from 'naive-ui'
 
 const inputText = ref("")
 
+// blocks
+
 const blockCnts = ref(1)
 const patterns = ref<[string, string, string][]>([["","",""]])
 const outputTexts = ref<string[]>([""])
-
-// function click(){
-//   console.log(outputTexts.value)
-//   console.log(patterns.value)
-// }
 
 function addBlock(){
   blockCnts.value += 1
@@ -28,37 +25,95 @@ function removeBlock(){
   outputTexts.value.pop()
 }
 
+// keys
+
+const keys = ref<string[]>([])
+const newKey = ref("")
+const addKeyStr = ref("save")
+function addKey(str: string){
+  let key = newKey.value
+  key = key.trim()
+  if (key === "") return
+  if (hasItem(key)) {
+    if (str === "save") {
+      addKeyStr.value = "overwrite?"
+      setTimeout(() => {addKeyStr.value = "save"}, 1000)
+      return
+    }
+    addKeyStr.value = "save"
+  }
+  if (addKeyStr.value == "overwrite?"){
+    console.log(key)
+  } else {
+    keys.value.push(key)
+  }  
+  save(key)
+  newKey.value = ""
+}
+function removeKey(index: number){
+  console.log(index)
+  keys.value.splice(index, 1)
+}
+function save(key: string){
+  const n = blockCnts.value
+  const patternArray = patterns.value
+  saveItem(key, { n, patternArray })
+}
+function load(key: string){
+  const { n, patternArray } = loadItem(key)
+  blockCnts.value = n
+  patterns.value = patternArray  
+}
 </script>
 
 <template>
-  <n-input
-    style="width: 100%" 
-    v-model:value="inputText"
-    type="textarea"
-    placeholder="inputText"
-    :autosize="{
-      minRows: 3,
-      maxRows: 50,
-    }"
-  />
-  <template v-for="i in range(blockCnts)" :key="i">
-    <block
-      :input-text="i==0 ? inputText : outputTexts[i-1]"
-      v-model:outputText="outputTexts[i]"
-      v-model:pattern="patterns[i]"
-    ></block>
-  </template>
-  
-  <n-button
-    @click="addBlock"
-  >
-    add one block
-  </n-button>
-  <n-button
-    @click="removeBlock"
-  >
-    remove last block
-  </n-button>
+  <span style="width:20%; display: list-item; text-align: left;">
+    <div v-for="key,index in keys" :key="key">
+      <n-button style="width:70%; justify-content:left" @click="load(key)">{{ key }}</n-button>
+      <n-button style="width:30%;" @click="removeKey(index)"> delete </n-button>
+    </div>
+    <div>
+      <n-input 
+        style="width:70%; justify-content:left"
+        v-model:value="newKey"
+      ></n-input>
+      <n-button
+        style="width:30%;"
+        @click="addKey(addKeyStr)"
+      > {{ addKeyStr }} </n-button>
+    </div>
+  </span>
+  <span style="width:80%; display: list-item">
+    <n-input
+      style="width: 100%" 
+      v-model:value="inputText"
+      type="textarea"
+      placeholder="inputText"
+      :autosize="{
+        minRows: 3,
+        maxRows: 50,
+      }"
+    />
+    <template v-for="i in range(blockCnts)" :key="i">
+      <block
+        :input-text="i==0 ? inputText : outputTexts[i-1]"
+        v-model:outputText="outputTexts[i]"
+        v-model:pattern="patterns[i]"
+      ></block>
+    </template>
+    
+    <n-button
+      @click="addBlock"
+    >
+      add one block
+    </n-button>
+    <n-button
+      @click="removeBlock"
+    >
+      remove last block
+    </n-button>
+  </span>
+
 </template>
 
 <style>
---
+++ src/functions/helpers.ts
@@ -0,0 +1,16 @@
+export function range(n: number) {
+  return Array.from(Array(n).keys());
+}
+
+export function hasItem(key: string) {
+  return localStorage.getItem(key) !== null
+}
+
+export function saveItem(key: string, obj:{n:number, patternArray:[string,string,string][]}) {
+  localStorage.setItem(key, JSON.stringify(obj));
+}
+
+export function loadItem(key: string) {
+  const obj:{n:number, patternArray:[string,string,string][]} = JSON.parse(localStorage.getItem(key));
+  return obj
+}
